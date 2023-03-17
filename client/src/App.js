58eb2c449b73508e70e23ec0a6dd4f1e065df645
hasanrc24 <hasanrc24@gmail.com>
2023-03-18 00:03:46 +0600

    route

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,19 @@
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "../src/pages/Home";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+
 function App() {
-  return <div className="text-blue-400">Hello chat</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Register />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
