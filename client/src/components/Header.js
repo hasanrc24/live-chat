540cb3687a89e6e160269d5875b65b4224f2f9d7
hasanrc24 <hasanrc24@gmail.com>
2023-03-20 14:03:45 +0600

    dropdown

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,8 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import { BiChevronDown } from "react-icons/bi";
 import { IoNotificationsSharp } from "react-icons/io5";
+import { useDispatch } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { addUserInfo } from "../redux/userSlice/userSlice";
 
-const Header = () => {
+const Header = ({ showMenu, setShowMenu }) => {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const handleLogout = () => {
+    dispatch(addUserInfo({}));
+    localStorage.removeItem("userInfo");
+    navigate("/login");
+  };
   return (
     <div className="bg-brand text-white p-4 flex justify-between items-center">
       <p className="font-semibold">Live Chat</p>
@@ -10,13 +21,27 @@ const Header = () => {
         <div className="flex items-center cursor-pointer">
           <IoNotificationsSharp className="h-5 w-5" />
         </div>
-        <div className="cursor-pointer flex items-center">
+        <div
+          // onBlur={() => setShowMenu(!showMenu)}
+          // onFocus={() => setShowMenu(!showMenu)}
+          onClick={() => setShowMenu(!showMenu)}
+          tabIndex="0"
+          className="cursor-pointer flex items-center  transition-all"
+        >
           <img
             src="https://icon-library.com/images/anonymous-avatar-icon/anonymous-avatar-icon-25.jpg"
             alt="profile img"
             className="h-8 w-8 rounded-full"
           />
-          <BiChevronDown className="h-7 w-7" />
+          <div className="relative">
+            <BiChevronDown className="h-7 w-7" />
+            {showMenu && (
+              <div className="absolute flex flex-col gap-2 right-0 top-11 py-2 px-3 text-white bg-brand z-50">
+                <p>Profile</p>
+                <p onClick={handleLogout}>Logout</p>
+              </div>
+            )}
+          </div>
         </div>
       </div>
     </div>
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import ChatLeft from "../components/ChatLeft";
 import ChatRight from "../components/ChatRight";
 import Header from "../components/Header";
@@ -9,18 +9,23 @@ import { useNavigate } from "react-router-dom";
 const Home = () => {
   const dispatch = useDispatch();
   const navigate = useNavigate();
+  const [showMenu, setShowMenu] = useState(false);
 
   useEffect(() => {
     if (localStorage.getItem("userInfo")) {
-      dispatch(addUserInfo(JSON.parse(localStorage.getItem("userInfo"))));
+      const info = JSON.parse(localStorage.getItem("userInfo"));
+      dispatch(addUserInfo(info));
     } else {
       navigate("/login");
     }
   }, []);
   return (
-    <div className="bg-brand-bg h-screen flex justify-center items-center">
+    <div
+      // onClick={() => setShowMenu(false)}
+      className="bg-brand-bg h-screen flex justify-center items-center"
+    >
       <div className="h-[80%] w-[70%] bg-white rounded-xl shadow-xl overflow-hidden">
-        <Header />
+        <Header setShowMenu={setShowMenu} showMenu={showMenu} />
         <div className="grid md:grid-cols-3 sm:grid-col-1 h-full">
           <div className="border-r-2 h-0 md:h-full">
             <ChatLeft />
